1284652528b916fb074a5f00fd1dbaa45955bb36
washucode <estherw8525@gmail.com>
2023-02-16 13:27:55 +0300

    add a C program that prints using put betty edits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
 *main - Main funtion of this project
 *
+*
+*
 *Return : Always 0 (Success)
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
