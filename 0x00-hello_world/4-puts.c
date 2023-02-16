e81f8c71c1dbd3b81a08c090bd8e5bdfbc368f17
washucode <estherw8525@gmail.com>
2023-02-16 13:07:21 +0300

    add a C program that prints using put add comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  *main - Main funtion of this project
  *
- *Return : return 0 
- *This program with enable a user to print an output using puts()
+ *Return : return 0 everytime
 */
 int main(void)
 {
   
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
