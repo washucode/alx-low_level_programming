a795874805be56228a12672bc799cd1a1fe869b2
washucode <estherw8525@gmail.com>
2023-02-17 17:19:37 +0300

    fix indentation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	l = n % 10;
 	if (l > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	else if (l != 0 && l < 6)
 	{
