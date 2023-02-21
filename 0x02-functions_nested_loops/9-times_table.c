47b9edd35b476157f98ff330848ad0ec5c00546f
washucode <estherw8525@gmail.com>
2023-02-21 23:54:44 +0300

    Learn your tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - prototype
+ *
+ *
+ *
+ * Result: none
+ */
+
+void times_table(void)
+{
+	int num, multiple, multiplied;
+
+	for (num = 0;  num <= 9; num++)
+	{
+		_putchar('0');
+		for (multiple = 0; multiple <= 9; multiple++)
+		{
+			_putchar(44);
+			_putchar(32);
+			multiplied = num * multiple;
+			if (multiplied <= 9)
+			{
+				_putchar(32);
+				_putchar(multiplied + '0');
+			}
+			else
+			{
+				_putchar((multiplied / 10) + '0');
+				_putchar((multiplied % 10) + '0');
+			}
+		}
+		_putchar(10);
+	}
+}
