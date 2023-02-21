bd274e21ad295f06a8688d16c3f4981a90de8f96
washucode <estherw8525@gmail.com>
2023-02-21 14:49:33 +0300

    1. Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - main prototype function
+ * desc:  prints alphabet letters
+ * Return: no Return
+ */
+void print_alphabet(void)
+{
+
+	 int a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	 _putchar(10);
+
+}
