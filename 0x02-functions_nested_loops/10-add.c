66abc4a6f02def6c02cae0e861b8ed107e944ca2
washucode <estherw8525@gmail.com>
2023-02-22 00:25:53 +0300

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - protoype to add tow integers
+ *
+ * @int1 : first integer
+ * @int2:second integer
+ * Return: sum of two integers
+ */
+
+int add(int int1, int int2)
+{
+	int sum_of_integers = int1 + int2;
+
+	return (sum_of_integers);
+}
+
