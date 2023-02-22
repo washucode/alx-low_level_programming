3b78869219bc34135457fe46ee6a28b56a34df33
washucode <estherw8525@gmail.com>
2023-02-22 03:26:18 +0300

    103 fibonnaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints sum o even fibonnaci
+ * Return: return 0
+ */
+
+
+int main(void)
+{
+unsigned long int1 = 0, int2 = 1, int3 = 0, sum = 0;
+while (int3 <= 4000000)
+{
+int3 = int1 + int2;
+int1 = int2;
+int2 = int3;
+
+if ((int1 % 2) == 0)
+sum += int1
+}
+printf("%1d\n", sum);
+return (0);
+}
