2fb7fd0585cdd27ee6a32d0d59cb5f9e18d3a64a
washucode <estherw8525@gmail.com>
2023-02-23 18:15:02 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar(10);
+
+}
