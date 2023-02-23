5248329bcae01813b8cc9d3039ab2bbb80a112c2
washucode <estherw8525@gmail.com>
2023-02-23 16:12:12 +0300

    0. Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - entry point of this program
+ * Description: checks if a number is positive negative or zeri
+ * @n - character to be checked
+ * Return: 0
+ */
+int positive_or_negative(int n)
+{
+	
+
+	
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
