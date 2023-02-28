bc12cd4cb2396c910b1c92476138e5039a3e0924
washucode <estherw8525@gmail.com>
2023-02-28 03:36:09 +0300

    6-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ *@str: the string to print.
+ */
+
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0 ; str[c] != '\0' ; c++)
+	{
+		if (str[c] % 2 != 0)
+			continue;
+		_putchar(str[c]);
+	}
+	_putchar(10);
+}
