c44cf88f11c51bc1d65bff0395cea80c57cd1915
washucode <estherw8525@gmail.com>
2023-03-07 02:52:18 +0300

    fix error

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0, s1 = 0 , s2 = 0;
+	int i = 0, s1 = 0,  s2 = 0;
 
 	while (i < size)
 	{
-	  s1 += *(a + (size * i  + i));
-	  s2 += *(a + (size  * i + size - 1 - i));
+		s1 += *(a + (size * i  + i));
+		s2 += *(a + (size  * i + size - 1 - i));
+		i++;
 	}
 	printf("%d, ", s1);
 	printf("%d\n", s2);
