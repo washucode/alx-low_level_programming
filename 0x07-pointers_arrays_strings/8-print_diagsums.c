f1f09fa5312a2cd02afa464d8458eb343d64c9c6
washucode <estherw8525@gmail.com>
2023-03-07 02:44:09 +0300

    seven

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  prints diagonal sum
+ * @a: pointer
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0, s1 = 0 , s2 = 0;
+
+	while (i < size)
+	{
+	  s1 += *(a + (size * i  + i));
+	  s2 += *(a + (size  * i + size - 1 - i));
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
