6ba484dcd98907b962c5d5348f02cbb6edd53df4
washucode <estherw8525@gmail.com>
2023-03-21 02:42:12 +0300

    TASK3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,9 @@ struct dog
 /**
  * dog_type - Typedef for dog structure
  */
-typedef struct dog dog_type;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
