9a6efa147bd18180f3cd78d8191466f00869bf23
washucode <estherw8525@gmail.com>
2023-03-20 23:10:30 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_type - Typedef for dog structure
+ */
+typedef struct dog dog_type;
+
+#endif
