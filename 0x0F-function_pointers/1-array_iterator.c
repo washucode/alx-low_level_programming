3cadaa78d2c97c0f8d5d869a57eefdca9e4ffb8d
washucode <estherw8525@gmail.com>
2023-03-23 03:15:43 +0300

    TASK 1 fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int x;
 
-	if (array == '\0' || action == '\0')
+	if (array == NULL  || action == NULL)
 		return;
 	for (x = 0; x < (int) size ; x++)
 	{
