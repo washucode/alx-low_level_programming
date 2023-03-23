7b560c34bce0c9d5dd7844dabb7402a1df56b1d9
washucode <estherw8525@gmail.com>
2023-03-23 05:18:07 +0300

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,14 +18,14 @@ int (*get_op_func(char *s))(int, int)
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
+
 	int i = 0;
 
 	while (i < 5)
 	{
 		if (strcmp(s, options[i].op) == 0)
-			return (options[i].f);	
+			return (options[i].f);
 		i++;
 	}
-
 	return (0);
 }
