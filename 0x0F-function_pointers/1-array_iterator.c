a43cc0005ee1c49964485b0330d66a4ff274207b
washucode <estherw8525@gmail.com>
2023-03-23 03:26:30 +0300

    TASK 1 fix2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,11 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-
 /**
  * array_iterator - execute function on each element
  * of an array
  * @array: ....
  * @size: size of the array
  * @action: pointer to function
- * Return: nothingx
+ * Return: nothing
  */
 
 
