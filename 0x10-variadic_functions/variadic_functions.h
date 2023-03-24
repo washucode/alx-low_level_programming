043028445da38f5b37e0abde78a880fc792adba4
washucode <estherw8525@gmail.com>
2023-03-24 05:04:15 +0300

    TASK 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,23 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct s_format - function object
+ * @format: printing format
+ * @f : function
+ */
+typedef struct s_format
+{
+  char format;
+  void (*f)(va_list *ar_list);
+}opt_l;
+
 
 #endif
