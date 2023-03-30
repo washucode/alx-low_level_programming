d6870c8b3d972d6b2fab261ca46df248b571f585
washucode <estherw8525@gmail.com>
2023-03-30 23:09:20 +0300

    TASK 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - return len of linked list
+ * @h : pointer to linkedlist
+ * Return: number fo elements on linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
