98982b3de014ebe504c4fa03fb95c6cc38be2483
washucode <estherw8525@gmail.com>
2023-04-04 15:15:48 +0300

    TASK 1 fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,15 @@
 
 /**
  * listint_len - returns number of elements
+ *
  * @h: pointer to head
+ *
  * Return: number of elements
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t total_elem;
+	size_t total_elem  = 0;
 
 	while (h != NULL)
 	{
