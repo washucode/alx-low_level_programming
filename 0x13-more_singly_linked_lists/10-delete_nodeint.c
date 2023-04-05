20d1b2b9dab43c86287a0a84d1f5481147818eb4
washucode <estherw8525@gmail.com>
2023-04-05 04:48:38 +0300

    TASK 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - ...
+ * @head: ...
+ * @index: ...
+ *
+ * Return: ...
+ */
+
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *deletedNode, *holder;
+
+	holder = *head;
+	deletedNode = malloc(sizeof(listint_t));
+	if (holder == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(holder);
+		return (1);
+	}
+	else
+	{
+		while (--index > 0)
+		{
+			if (holder->next == NULL)
+				return (-1);
+			holder = holder->next;
+		}
+		deletedNode = holder->next;
+		holder->next = deletedNode->next;
+		free(deletedNode);
+		return (1);
+	}
+}
