506fda9d82387d9d6cbf8a29d50850ec3f71c728
washucode <estherw8525@gmail.com>
2023-04-09 16:46:25 +0300

    TASK 3 fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * set_bit - set bit
+ *
+ *@n: bit to set
+ * @index: index of bit to set
+ * Return: -1 or 1
+ */
+
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -7,9 +15,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	if (*n & (1 << index))
 		return (1);
-	else
-	{
-		*n =*n | (1 << index);
-		return (1);
-	}
+	*n = *n | (1 << index);
+	return (1);
 }
