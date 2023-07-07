70d42880baba6c0456c12de845b2ad42f1513ba3
washucode <estherw8525@gmail.com>
2023-07-08 01:15:53 +0300

    task 2 fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,8 +11,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = 0;
-
-	hash = hash_djb2(key);
-	return (hash % size);
+	return (hash_djb2(key) % size);
 }
